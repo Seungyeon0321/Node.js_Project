307bae7c21b27e4fe09d44687a3075e6703d82b9
Seungyeon0321 <71824405+Seungyeon0321@users.noreply.github.com>
2024-08-25 00:38:18 -0700

    feat: Added nodemailer feature

--- utils/email.js
+++ utils/email.js
@@ -17,8 +17,16 @@ module.exports = class Email {
 
   newTransport() {
     if (process.env.NODE_ENV === 'production') {
-      // Sendgrid
-      return 1;
+      // Brevo
+      return nodemailer.createTransport({
+        host: 'smtp-relay.brevo.com',
+        port: 587,
+        secure: false, // true for 465, false for other ports
+        auth: {
+          user: process.env.BREVO_EMAIL, // generated ethereal user
+          pass: process.env.BREVO_KEY, // generated ethereal password
+        },
+      });
     }
 
     return nodemailer.createTransport({
@@ -52,11 +60,23 @@ module.exports = class Email {
     console.log(mailOptions);
 
     // 3) Create a transport and send email
-    await this.newTransport().sendMail(mailOptions);
+    await this.newTransport().sendMail(mailOptions, (error, info) => {
+      if (error) {
+        console.log(error);
+      } else {
+        console.log(info);
+      }
+    });
   }
 
   async sendWelcome() {
-    console.log('whatttttttttt?');
     await this.send('welcome', 'Welcome to the Natours Family!');
+  }
+
+  async sendPasswordReset() {
+    await this.send(
+      'passwordReset',
+      'Your password reset token (valid for only 10 minutes)',
+    );
   }
 };
